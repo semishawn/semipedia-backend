8770303e3ac635147bbc774e537cc43ab03a1ace
semishawn <45698497+semishawn@users.noreply.github.com>
2020-08-24 23:03:25 -0400

    added database access

--- web/index.php
+++ web/index.php
@@ -1,25 +1,30 @@
 <?php
 
-require('../vendor/autoload.php');
+$dbopts = parse_url(getenv('DATABASE_URL'));
+$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
+	array(
+		'pdo.server' => array(
+			'driver'   => 'pgsql',
+			'user' => $dbopts["user"],
+			'password' => $dbopts["pass"],
+			'host' => $dbopts["host"],
+			'port' => $dbopts["port"],
+			'dbname' => ltrim($dbopts["path"],'/')
+		)
+	)
+);
 
-$app = new Silex\Application();
-$app['debug'] = true;
+$app->get('/db/', function() use($app) {
+	$st = $app['pdo']->prepare('SELECT name FROM test_table');
+	$st->execute();
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+	$names = array();
+	while ($row = $st->fetch(PDO::FETCH_ASSOC)) {
+		$app['monolog']->addDebug('Row ' . $row['name']);
+		$names[] = $row;
+	}
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+	return $app['twig']->render('database.twig', array(
+		'names' => $names
+	));
 });
-
-$app->run();
