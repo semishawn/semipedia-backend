07250431291baada775527530d3687bb022c3854
semishawn <45698497+semishawn@users.noreply.github.com>
2020-08-25 02:55:10 -0400

    full implementation

--- polls/open-ended/onsubmit.php
+++ polls/open-ended/onsubmit.php
@@ -3,15 +3,20 @@ header("Access-Control-Allow-Origin: *");
 
 $answer = $_REQUEST['answer'];
 
-$file = 'count.txt';
+$list_file = 'answer-list.txt';
+$count_file = 'answer-count.txt';
+
+$date = new DateTime("now", new DateTimeZone('America/New_York'));
+$date = $date->format('M j, Y \a\t g:ia');
+
+$fp = fopen($list_file, 'a');
+fwrite($fp, $date."\n");
+fwrite($fp, '"'.$answer.'"'."\n\n");
+fclose($fp);
 
 $count = 1;
-   
-if (file_exists($file)) {
-    $count += file_get_contents($file);
-}
-
-file_put_contents($file, $count);
+$count += file_get_contents($count_file);
+file_put_contents($count_file, $count);
 ?>
 
 <div class="poll-title">Thanks!</div>
