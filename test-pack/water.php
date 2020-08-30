565615591bb89044ef353fb84ca744c162c4bb98
semishawn <45698497+semishawn@users.noreply.github.com>
2020-08-30 01:13:54 -0400

    Update water.php

--- test-pack/water.php
+++ test-pack/water.php
@@ -11,8 +11,9 @@ $type = "open_ended";
 date_default_timezone_set('America/New_York');
 $date = date("M j, Y") ." at ". date("g:ia");
 
-pg_query($conn,"INSERT INTO {$type} (date, answer) VALUES ('{$data}', '{$answer}')");
-$count = pg_query($conn,"SELECT COUNT(*) FROM {$type}");
+pg_query($conn,"INSERT INTO {$type} (date, answer) VALUES ('{$date}', '{$answer}')");
+$rows = pg_query($conn,"SELECT * FROM {$type}");
+$count = pg_num_rows($rows);
 
 function answerTotal() {
 	global $count;
