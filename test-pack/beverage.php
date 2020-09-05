854da61f1076074d37f19a4a285b3934e7196dad
semishawn <45698497+semishawn@users.noreply.github.com>
2020-09-04 23:48:38 -0400

    Update beverage.php

--- test-pack/beverage.php
+++ test-pack/beverage.php
@@ -1,5 +1,4 @@
 <?php
-
 header("Access-Control-Allow-Origin: *");
 
 $vote = $_REQUEST["vote"];
@@ -7,13 +6,13 @@ $vote = $_REQUEST["vote"];
 $conn = pg_connect(getenv("DATABASE_URL"));
 
 $type = "multi_choice";
-$title = "'beverage'";
+$title = "beverage";
 
-if ($vote == 1) {pg_query($conn, "UPDATE {$type} SET option1 = option1 + 1 WHERE title = {$title}");}
-if ($vote == 2) {pg_query($conn, "UPDATE {$type} SET option2 = option2 + 1 WHERE title = {$title}");}
-if ($vote == 3) {pg_query($conn, "UPDATE {$type} SET option3 = option3 + 1 WHERE title = {$title}");}
+if ($vote == 1) {pg_query($conn, "UPDATE {$type} SET option1 = option1 + 1 WHERE title = '{$title}'");}
+if ($vote == 2) {pg_query($conn, "UPDATE {$type} SET option2 = option2 + 1 WHERE title = '{$title}'");}
+if ($vote == 3) {pg_query($conn, "UPDATE {$type} SET option3 = option3 + 1 WHERE title = '{$title}'");}
 
-$result = pg_query($conn, "SELECT * FROM {$type} WHERE title = {$title}");
+$result = pg_query($conn, "SELECT * FROM {$type} WHERE title = '{$title}'");
 $row = pg_fetch_array($result, NULL, PGSQL_ASSOC);
 $option1 = $row["option1"];
 $option2 = $row["option2"];
@@ -33,39 +32,17 @@ function voteTotal() {
 		echo($count." votes");
 	};
 };
+?>
 
-?> 
+<script>
+$('.submit').click(function() {
+	$('.option-percent:nth-child(1)').html('<?php echo $vote1;?>');
+	$('.option-fill:nth-child(1)').animate({width: <?php echo $vote1;?>}, 500);
+	$('.option-percent:nth-child(2)').html('<?php echo $vote2;?>');
+	$('.option-fill:nth-child(2)').animate({width: <?php echo $vote2;?>}, 500);
+	$('.option-percent:nth-child(3)').html('<?php echo $vote3;?>');
+	$('.option-fill:nth-child(3)').animate({width: <?php echo $vote3;?>}, 500);
 
-<style>
-.multi-choice label {pointer-events: none;}
-
-.fill1 {animation: fill1 0.5s ease-in-out forwards;}
-.fill2 {animation: fill2 0.5s ease-in-out forwards;}
-.fill3 {animation: fill3 0.5s ease-in-out forwards;}
-
-@keyframes fill1 {from{width: 0%;} to{width: <?php echo($vote1);?>%;}}
-@keyframes fill2 {from{width: 0%;} to{width: <?php echo($vote2);?>%;}}
-@keyframes fill3 {from{width: 0%;} to{width: <?php echo($vote3);?>%;}}
-</style>
-
-<div class="poll-title">Thanks!</div>
-
-<label>
-	<div class="vote-fill fill1"></div>
-	<span class="option"><?php echo($vote1);?>%</span>
-</label>
-
-<label>
-	<div class="vote-fill fill2"></div>
-	<span class="option"><?php echo($vote2);?>%</span>
-</label>
-
-<label>
-	<div class="vote-fill fill3"></div>
-	<span class="option"><?php echo($vote3);?>%</span>
-</label>
-
-<div class="poll-bottom">
-	<div class="poll-count"><?php voteTotal();?></div>
-	<button class="submit" disabled>Submit</button>
-</div>
+	$('.poll-count').html('<?php voteTotal();?>');
+});
+</script>
