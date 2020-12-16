abe0c4cd7ab08beefc6fa88cb6d9893d71785d10
semishawn <45698497+semishawn@users.noreply.github.com>
2020-12-16 11:23:45 -0500

    quotation fix?

--- packs/test/beverage.php
+++ packs/test/beverage.php
@@ -9,8 +9,8 @@ $title = 'beverage';
 $vote = $_REQUEST['vote'];
 $conn = pg_connect(getenv('DATABASE_URL'));
 
-pg_query($conn, 'UPDATE {$type} SET option{$vote} = option{$vote} + 1 WHERE title = "{$title}"');
-$result = pg_query($conn, 'SELECT * FROM {$type} WHERE title = "{$title}"');
+pg_query($conn, "UPDATE $type SET option$vote = option$vote + 1 WHERE title = '$title'");
+$result = pg_query($conn, "SELECT * FROM $type WHERE title = '$title'");
 $row = pg_fetch_array($result, NULL, PGSQL_ASSOC);
 
 $option1 = $row['option1'];
