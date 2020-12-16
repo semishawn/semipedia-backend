c87bad97cdfca39505d8588f1096d4c38b461007
semishawn <45698497+semishawn@users.noreply.github.com>
2020-12-16 10:57:15 -0500

    Create marvel.php

---
+++ packs/test/marvel.php
@@ -0,0 +1,20 @@
+<?php
+$type = 'multi_choice4';
+$title = 'marvel';
+
+header('Access-Control-Allow-Origin: *');
+$vote = $_REQUEST['vote'];
+$conn = pg_connect(getenv('DATABASE_URL'));
+
+pg_query($conn, 'UPDATE {$type} SET option{$vote} = option{$vote} + 1 WHERE title = "{$title}"');
+$result = pg_query($conn, 'SELECT * FROM {$type} WHERE title = "{$title}"');
+$row = pg_fetch_array($result, NULL, PGSQL_ASSOC);
+
+$option1 = $row['option1'];
+$option2 = $row['option2'];
+$option3 = $row['option3'];
+$option4 = $row['option3'];
+$options = [$option1, $option2, $option3, $option4];
+
+echo json.encode($options);
+?>
