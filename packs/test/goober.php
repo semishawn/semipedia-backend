3dcb7ecc118fcaf909b4a2ece3d05607f160b036
semishawn <45698497+semishawn@users.noreply.github.com>
2020-12-22 21:11:58 -0500

    likert scale poll test

---
+++ packs/test/goober.php
@@ -0,0 +1,24 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+ini_set('display_errors', 1); 
+error_reporting(E_ALL);
+
+$type = 'likert';
+$title = 'goober';
+
+$agreement = $_REQUEST['vote'];
+$conn = pg_connect(getenv('DATABASE_URL'));
+
+pg_query($conn, "UPDATE $type SET point$point = point$point + 1 WHERE title = '$title'");
+$result = pg_query($conn, "SELECT * FROM $type WHERE title = '$title'");
+$row = pg_fetch_array($result, NULL, PGSQL_ASSOC);
+
+$point1 = intval($row['point1']);
+$point2 = intval($row['point2']);
+$point3 = intval($row['point3']);
+$point4 = intval($row['point4']);
+$point5 = intval($row['point5']);
+$points = [$point1, $point2, $point3, $point4, $point4];
+
+echo json_encode($points);
+?>
--- queries.sql
+++ queries.sql
@@ -24,3 +24,22 @@ CREATE TABLE open_ended (
 INSERT INTO open_ended (date, answer) VALUES ('Aug 28, 2020 6:42am', 'My ass is so god damn huge');
 --- Make title primary
 ALTER TABLE open_ended ADD PRIMARY KEY (date);
+
+
+
+--- Likert Scale:
+--- Create table
+CREATE TABLE likert (
+  title text NOT NULL,
+  point1 int NOT NULL DEFAULT 0,
+  point2 int NOT NULL DEFAULT 0,
+  point3 int NOT NULL DEFAULT 0,
+  point4 int NOT NULL DEFAULT 0,
+  point5 int NOT NULL DEFAULT 0
+);
+--- Set initial values
+INSERT INTO likert (title, point1, point2, point3, point4, point5) VALUES ('my poll', 0, 0, 0, 0, 0);
+--- Make title primary
+ALTER TABLE likert ADD PRIMARY KEY (title);
+--- Reset/alter values
+UPDATE likert SET point1 = 0, point2 = 0, point3 = 0, point4 = 0, point5 = 0 WHERE title = 'my poll';
